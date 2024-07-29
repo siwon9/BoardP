244768c9261b51a25824a097004666c1a17b74c9
siwon9 <siwon10823@gmail.com>
2024-07-29 12:45:07 +0900

    비밀번호 validator default 메서드 정의

---
+++ project/src/main/java/org/siwon/global/validators/PasswordValidator.java
@@ -0,0 +1,36 @@
+package org.siwon.global.validators;
+
+public interface PasswordValidator { // 복잡성을 가감하기 위한 인터페이스
+    /**
+     * 알파벳 복잡성 체크
+     * @param password
+     * @param caseIncensitive - false : 대소문자 각각 1개씩 이상 포함, true - 대소문자 구분 X
+     * @return
+     */
+    default boolean alphaCheck(String password, boolean caseIncensitive) {
+
+        return false;
+    }
+
+    /**
+     * 숫자 복잡성 체크
+     * @param password
+     * @return
+     */
+    default boolean numberCheck(String password) {
+
+        return false;
+    }
+
+
+    /**
+     * 특수문자 복잡성 체크
+     * @param password
+     * @return
+     */
+    default boolean specialCharsCheck(String password) {
+
+        return false;
+    }
+}
+
--- project/src/main/java/org/siwon/members/controllers/MemberController.java
+++ project/src/main/java/org/siwon/members/controllers/MemberController.java
@@ -1,6 +1,8 @@
 package org.siwon.members.controllers;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.siwon.members.validators.JoinValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final JoinValidator joinValidator;
 
     @GetMapping("/join")
     public String join(@ModelAttribute RequestJoin form) {
@@ -20,6 +25,8 @@ public class MemberController {
 
     @PostMapping("/join")
     public String joinPs(@Valid RequestJoin form, Errors errors) {
+
+        joinValidator.validate(form, errors);
 
         if(errors.hasErrors()){
             return "front/member/join";
