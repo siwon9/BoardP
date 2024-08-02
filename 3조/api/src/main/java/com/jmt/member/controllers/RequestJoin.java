05759fb94c765eae89c63ca15a8703bac29972ac
siwon9 <siwon10823@gmail.com>
2024-08-02 16:28:09 +0900

    8월 2일 수업내용

---
+++ 3조/api/src/main/java/com/jmt/member/controllers/RequestJoin.java
@@ -0,0 +1,26 @@
+package com.jmt.member.controllers;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class RequestJoin {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank @Size(min=8)
+    private String password;
+
+    @NotBlank
+    private String confirmPassword;
+
+    @NotBlank
+    private String userName;
+
+    @NotBlank
+    private String mobile;
+
+    @AssertTrue
+    private boolean agree;
+}
